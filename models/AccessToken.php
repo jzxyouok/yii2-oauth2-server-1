6a9d27c391f7f1b1cfd0058535635a26a5a0be52
xuchao <85825770@qq.com>
2017-03-22 13:40:49 +0800

    ccc

---
+++ models/AccessToken.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\oauth2\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * Access Token Repository Interface
+ * @package yuncms\oauth2\models
+ */
+class AccessToken extends ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%oauth2_access_token}}';
+    }
+
+    /**
+     * 定义应用和用户的关系
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(Yii::$app->user->identityClass, ['id' => 'user_id']);
+    }
+}
